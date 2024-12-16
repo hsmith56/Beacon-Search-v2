8f1eeda6ce18b00c62f2f073fc854a9b4ca01a39
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2024-12-16 17:08:51 -0500

    added previous html files, eventually think about serving a dedicated frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app = FastAPI(docs_url=None, redoc_url=None)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
