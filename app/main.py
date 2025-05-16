954c5a3eb21856bcd3bbac0509d66e2205c09967
harrison <hsmith56@pm.me>
2025-05-16 11:21:30 -0400

    clean start

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
